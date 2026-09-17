a526a349c87dfe2a4ed97a10e63f2eb8f75f9e35
BLACKOUT088 <noah.ludwig2011@icloud.com>
2026-09-17 10:36:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let punkte=1;
 const button=document.getElementById("CPS");
-button.addEventListener("Click", funktion() {
+button.addEventListener("click", function() {
   punkte++;
   button.textContent=punkte;
 });
