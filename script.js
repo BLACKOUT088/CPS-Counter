834dd21c420226488167251c42e77a6ddfedf321
BLACKOUT088 <noah.ludwig2011@icloud.com>
2026-09-23 20:35:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ button.addEventListener("click", function() {
   punkte++;
   button.textContent=punkte;
   setTimeout(function() {
-    time.classList.toggle("aus");
+    time.classList.toggle("an");
   }, 100);
 });
