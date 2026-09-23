4c000c88c9a3a780e0e14e5fa85142842cd92a13
BLACKOUT088 <noah.ludwig2011@icloud.com>
2026-09-23 15:38:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let punkte=0;
 const button=document.getElementById("CPS");
+const time=document.getElementById("Zeit")
 button.addEventListener("click", function() {
   punkte++;
   button.textContent=punkte;
+  setTimeout(function() {
+    time.classList.toggle("aus");
+  }, 100);
 });
