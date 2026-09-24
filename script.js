a4f0bb8748e591c6d8f6425acaf07b8fd214e759
BLACKOUT088 <noah.ludwig2011@icloud.com>
2026-09-24 20:34:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-let punkte=0;
-const button=document.getElementById("CPS");
-const time=document.getElementById("Zeit")
-const result=document.getElementById("End")
-button.addEventListener("click", function() {
-  punkte++;
-  button.textContent=punkte;
-  setTimeout(function() {
-    time.textContent=punkte
-    result.classList.toggle("An");
-  }, 100);
+let punkte = 0;
+let testLaeuft = false;
+
+const button = document.getElementById("CPS");
+const time = document.getElementById("Zeit");
+const result = document.getElementById("End");
+
+button.addEventListener("click", function () {
+  // Der erste Klick startet den Test
+  if (testLaeuft === false) {
+    testLaeuft = true;
+    punkte = 0;
+    button.textContent = "0";
+
+    // Nach 1 Sekunde wird der Test beendet
+    setTimeout(function () {
+      testLaeuft = false;
+      button.disabled = true;
+
+      time.textContent = punkte;
+      result.style.display = "block";
+      button.textContent = "Fertig";
+    }, 1000);
+  }
+
+  // Jeder Klick wird gezählt
+  if (testLaeuft === true) {
+    punkte++;
+    button.textContent = punkte;
+  }
 });
